67f0170fbccc21587bf0376984109e0323041c97
robot <email@email.com>
2026-06-28 02:16:34 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nixos-generate-config --show-hardware-config --no-filesystems > hardware-configuration.nix
+#nixos-generate-config --show-hardware-config --no-filesystems > hardware-configuration.nix
 sudo nix --extra-experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --flake .#nixos --disk main /dev/sdb
