78a5d3e033c351b145e5fa2842fb6141f89d230e
robot <email@email.com>
2026-06-28 04:04:07 +0100

    minor fix

--- modules/disko.nix
+++ modules/disko.nix
@@ -11,72 +11,69 @@
       };
     };
 
-    disko.devices.disk.main = {
-      device = "/dev/sdb";
-      type = "disk";
+    disko.devices = {
+      disk = {
+        sdb = {
+          device = "/dev/sdb";
+          type = "disk";
 
-      content.type = "gpt";
+          content.type = "gpt";
 
-      content.partitions.boot = {
-        name = "boot";
-        size = "1M";
-        type = "EF02";
-      };
-
-      content.partitions.esp = {
-        name = "ESP";
-        size = "1G";
-        type = "EF00";
-
-        content = {
-          type = "filesystem";
-          format = "vfat";
-          mountpoint = "/boot";
-        };
-      };
-
-      content.partitions.swap = {
-        size = "4G";
-
-        content = {
-          type = "swap";
-          resumeDevice = true;
-        };
-      };
-      luks = {
-        size = "100%";
-        label = "luks";
-        content = {
-          type = "luks";
-          name = "cryptroot";
-          extraOpenArgs = [
-            "--allow-discards"
-            "--perf-no_read_workqueue"
-            "--perf-no_write_workqueue"
-          ];
-          # https://0pointer.net/blog/unlocking-luks2-volumes-with-tpm2-fido2-pkcs11-security-hardware-on-systemd-248.html
-          settings = {
-            crypttabExtraOpts = [ "fido2-device=auto" "token-timeout=10" ];
+          content.partitions.boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02";
           };
-          content = {
-            type = "btrfs";
-            extraArgs = [ "-f" ];
 
-            subvolumes = {
+          content.partitions.esp = {
+            name = "ESP";
+            size = "1G";
+            type = "EF00";
 
-              "/root" = {
-                mountpoint = "/";
-                mountOptions = [ "subvol=root" "compress=zstd" "noatime" ];
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+
+          };
+          content.luks = {
+            size = "100%";
+            label = "luks";
+            content = {
+              type = "luks";
+              name = "cryptroot";
+              extraOpenArgs = [
+                "--allow-discards"
+                "--perf-no_read_workqueue"
+                "--perf-no_write_workqueue"
+              ];
+              # https://0pointer.net/blog/unlocking-luks2-volumes-with-tpm2-fido2-pkcs11-security-hardware-on-systemd-248.html
+              settings = {
+                crypttabExtraOpts = [ "fido2-device=auto" "token-timeout=10" ];
               };
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
 
-              "/persistent" = {
-                mountOptions = [ "subvol=persistent" "noatime" ];
-                mountpoint = "/persistent";
-              };
+                subvolumes = {
 
-              "/nix" = {
-                mountOptions = [ "subvol=nix" "noatime" ];
-                mountpoint = "/nix";
+                  "/root" = {
+                    mountpoint = "/";
+                    mountOptions = [ "subvol=root" "compress=zstd" "noatime" ];
+                  };
+
+                  "/persistent" = {
+                    mountOptions = [ "subvol=persistent" "noatime" ];
+                    mountpoint = "/persistent";
+                  };
+
+                  "/nix" = {
+                    mountOptions = [ "subvol=nix" "noatime" ];
+                    mountpoint = "/nix";
+                  };
+                };
               };
             };
           };
