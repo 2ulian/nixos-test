24ed612c7e720254f82a40679fa2654c6c62d0ff
robot <email@email.com>
2026-06-28 06:01:58 +0100

    minor fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #nixos-generate-config --show-hardware-config --no-filesystems > hardware-configuration.nix
-sudo nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko --flake .#nixos --disk main /dev/sdb
-sudo mkdir -p /mnt/tmp
-sudo TMPDIR=/mnt/tmp nixos-install --root /mnt --flake .#nixos
+sudo nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko ./modules/disko.nix --argstr disk /dev/sdb
+#sudo mkdir -p /mnt/tmp
+#sudo TMPDIR=/mnt/tmp nixos-install --root /mnt --flake .#nixos
